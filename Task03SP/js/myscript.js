6e9d5de4e79c09473a5178437f8077137e936eda
Dwipuspa <63333393+Dwipuspa@users.noreply.github.com>
2020-05-12 16:47:41 +0700

    Add files via upload

--- Task03SP/js/myscript.js
+++ Task03SP/js/myscript.js
@@ -68,9 +68,9 @@ var slideIndex = 0,auto,i;
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className === "topnav clearfix") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "topnav clearfix";
   }
 }
